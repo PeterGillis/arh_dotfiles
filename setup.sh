4c820c33350cc7f25c895942a023fa58859a2441
Peter Gillis <peter.gillis@aroundhome.de>
2020-10-22 15:28:47 +0200

    Add setup file

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+export DOTFILES=`dirname "$0"`
+export DOTFILES=`readlink -f "$DOTFILES"`
+
+if [ ! -L "$HOME/.dotfiles" ]; then
+  echo "Creating symlink for dotfiles folder..."
+  [ -e "$HOME/.dotfiles" ] && rm "$HOME/.dotfiles"
+  ln -s "$DOTFILES" "$HOME/.dotfiles"
+fi
+
+if [ ! -d "$HOME/.rbenv" ]; then
+  echo "Installing rbenv..."
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  cd ~/.rbenv && src/configure && make -C src
+  mkdir -p ~/.rbenv/plugins
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars
+  git clone https://github.com/rbenv/rbenv-each.git ~/.rbenv/plugins/rbenv-each
+fi
+
+function link_file {
+  if [ ! -L "$HOME/$1" ]; then
+    echo "Creating $1..."
+    [ -e "$HOME/$1" ] && rm "$HOME/$1"
+    ln -s "$DOTFILES/$1" "$HOME/$1"
+  fi
+}
+
+link_file ".bashrc"
+link_file ".gemrc"
+link_file ".gitattributes"
+link_file ".gitconfig"
+link_file ".gitignore"
+link_file ".vimrc"
+
+if [ ! -e "$HOME/.git-completion.sh" ]; then
+  echo "Downloading git completion..."
+  wget -O "$HOME/.git-completion.sh" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+fi
